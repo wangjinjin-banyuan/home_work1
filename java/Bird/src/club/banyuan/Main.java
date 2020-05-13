cd9dae17d709e47f62555355e842d4517cfa4069
wangjinjin <874957911@qq.com>
2020-05-13 14:49:27 +0800

    5.12homework

---
+++ java/Bird/src/club/banyuan/Main.java
@@ -0,0 +1,26 @@
+package club.banyuan;
+
+public class Main {
+
+  public static void show(Bird bird) {
+
+    
+    System.out.println(bird.getName() + "能够飞 " + bird.fly() + "米");
+  }
+
+  public static void main(String[] args) {
+
+    show(new Bird() {
+      //TODO
+      {
+        super.setName("燕子");
+      }
+      @Override
+      public int fly() {
+
+        return 1000;
+      }
+    });
+
+  }
+}
